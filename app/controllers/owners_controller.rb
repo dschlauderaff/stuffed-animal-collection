59c0cc8554648585199faf3a92fe3034864d877c
Dave Schlauderaff <dschlaud@gmail.com>
2017-06-24 20:04:27 -0500

    login route created and tested working

--- app/controllers/owners_controller.rb
+++ app/controllers/owners_controller.rb
@@ -18,9 +18,24 @@ class OwnersController < ApplicationController
     end
   end
 
+  get '/login' do
+    if !logged_in?
+      erb :'users/login'
+    else
+      redirect '/' #TODO change this redirect to owner's index
+    end
+  end
+
+  post '/login' do
+    # binding.pry
+    owner = Owner.find_by(name: params[:owner][:name])
+
+    user_log_in(owner)
+  end
+
   get '/logout' do
     session.clear
-    redirect 'login'
+    redirect '/login'
   end
 
 end
