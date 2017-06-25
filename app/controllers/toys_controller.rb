e8810d0db77156548a6c141a3eef4fbf733fb697
Dave Schlauderaff <dschlaud@gmail.com>
2017-06-24 21:46:40 -0500

    created the get /toys/new and post /toys route, tested and adding toys to owner's page

--- app/controllers/toys_controller.rb
+++ app/controllers/toys_controller.rb
@@ -1,3 +1,21 @@
 class ToysController < ApplicationController
 
+  get '/toys/new' do
+    if logged_in?
+      erb :'toys/create_toys'
+    else
+      redirect '/login'
+    end
+  end
+
+  post '/toys' do
+    # binding.pry
+    toy = current_user.toys.build(params[:toy])
+    if toy.save
+      redirect "/owners/#{current_user.id}"
+    else
+      redirect '/failure' #TODO do something better with this
+    end
+  end
+
 end
